a3885ed37a4a98d9cb0deac700b4d88d13453dd6
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 13:12:03 -0800

    Animals
    Typescript 45Q assignment exercise 35

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+// Animals: Think of atleast three animal
+// List of animals with a common characteristic
+const animals = ['cat', 'dog', 'goat'];
+// printing the names of each animal using a for of loof
+console.log("List of animals:");
+for (const animal of animals) {
+    console.log(animal);
+}
+// Print a statement about each animal.
+console.log("\nStatements about each animal:");
+for (const animal of animals) {
+    if (animal === 'dog') {
+        console.log(`A ${animal} is a great pet.`);
+    }
+    else if (animal === 'cat') {
+        console.log(`A ${animal}would be a good companion at home.`);
+    }
+    else if (animal === 'goat') {
+        console.log(`A ${animal}is an adorable pet that also give us milk.`);
+    }
+}
+// Common characteristic.
+console.log("\nWhat these animals have in common:");
+console.log("Any of these animals could be a great pet!");
